ec38f7a2e910236b65211fa0a644d94c15a9318d
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-11-22 11:54:49 +0700

    UPDATE VITE CONFIRM

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,23 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: './',
+  server: {
+    host: '0.0.0.0' // Makes the server accessible externally
+  },
   resolve: {
     alias: {
       '@utils': path.resolve(__dirname, './src/utils'),
       '@core': path.resolve(__dirname, './src/core'),
       '@constants': path.resolve(__dirname, './src/constants')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
     }
   }
 })
